9ce0684507f29a1563a150c20c2efb95af7123e3
Harmeet Singh <er.harmeetsingh@hotmail.com>
2024-09-22 16:40:11 -0600

    Add k8s script for colima and minikube

---
+++ local-kubernetes.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+SESSION="LocalKubernetes"
+SESSIONEXISTS=$(tmux list-sessions | grep $SESSION)
+
+if [ "$SESSIONEXISTS" = "" ]
+then
+    tmux new-session -d -s $SESSION
+
+    tmux rename-window -t 0 'colima'
+    tmux send-keys -t colima 'colima start' Enter 
+    tmux send-keys -t colima 'colima status' Enter 
+
+    while ! tmux capture-pane -p -t $WINDOW_A.$PANE_A | grep "colima is running" ; do
+        sleep 1
+    done ;
+
+    # Create and setup pane for hugo server
+    tmux new-window -t $SESSION:1 -n 'minikube'
+    tmux send-keys -t minikube 'minikube start' Enter 
+
+    # tmux new-window -t $SESSION:2 -n 'Writing'
+    # tmux send-keys -t 'Writing' "nvim" C-m
+
+    # Setup an additional shell
+    # tmux new-window -t $SESSION:3 -n 'Shell'
+    # tmux send-keys -t 'Shell' "zsh" C-m 'clear' C-m
+fi
+
+tmux attach-session -t $SESSION:0
